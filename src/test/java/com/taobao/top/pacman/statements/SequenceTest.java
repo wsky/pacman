d913c9ca507b8914a80c42de463977ea7bf0388a
houkun <houkun@taobao.com>
2014-07-09 14:12:00 +0800

    update test

--- src/test/java/com/taobao/top/pacman/statements/SequenceTest.java
+++ src/test/java/com/taobao/top/pacman/statements/SequenceTest.java
@@ -3,21 +3,23 @@ package com.taobao.top.pacman.statements;
 import java.util.Map;
 
 import com.taobao.top.pacman.Activity;
+import com.taobao.top.pacman.Variable;
 import com.taobao.top.pacman.testsuite.StatementTestBase;
 
 public class SequenceTest extends StatementTestBase {
 	protected Activity createActivity() {
 		Sequence sequence = new Sequence();
+		sequence.getVariables().add(new Variable("var", 1));
 		sequence.getActivities().add(new WriteLine());
 		sequence.getActivities().add(new WriteLine());
 		return sequence;
 	}
-
+	
 	@Override
 	protected Map<String, Object> createInputs() {
 		return null;
 	}
-
+	
 	@Override
 	protected void assertOutputs(Map<String, Object> outputs) {
 	}
