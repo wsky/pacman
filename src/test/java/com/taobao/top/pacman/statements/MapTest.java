b2f27405625071e88d5c91ab7ddc60fb70a35677
houkun <houkun@taobao.com>
2014-07-11 11:24:39 +0800

    #8; impl GetFromMap and test

--- src/test/java/com/taobao/top/pacman/statements/MapTest.java
+++ src/test/java/com/taobao/top/pacman/statements/MapTest.java
@@ -27,27 +27,42 @@ public class MapTest extends StatementTestBase {
 		sequence.getVariables().add(key2);
 		sequence.getVariables().add(value);
 		
+		// add key1
 		AddToMap add1 = new AddToMap();
 		add1.Map = new InArgument(map);
 		add1.Key = new InArgument(key1);
 		add1.Value = new InArgument(value);
 		sequence.getActivities().add(add1);
 		
+		// add key2
 		AddToMap add2 = new AddToMap();
 		add2.Map = new InArgument(map);
 		add2.Key = new InArgument(key2);
 		add2.Value = new InArgument(value);
 		sequence.getActivities().add(add2);
 		
+		// remove key1
 		RemoveFromMap remove = new RemoveFromMap();
 		remove.Map = new InArgument(map);
 		remove.Key = new InArgument(key1);
 		sequence.getActivities().add(remove);
 		
+		// exists key1
 		ExistsInMap exists = new ExistsInMap();
 		exists.Map = new InArgument(map);
 		exists.Key = new InArgument(key1);
 		sequence.getActivities().add(exists);
+		
+		GetFromMap get = new GetFromMap();
+		get.setDisplayName("GetFromMap");
+		get.Map = new InArgument(map);
+		get.Key = new InArgument(key2);
+		// add key3, assign value of key2
+		AddToMap add3 = new AddToMap();
+		add3.Map = new InArgument(map);
+		add3.Key = new InArgument("key3");
+		add3.Value = new InArgument(get);
+		sequence.getActivities().add(add3);
 		
 		return sequence;
 	}
@@ -62,6 +77,7 @@ public class MapTest extends StatementTestBase {
 		System.out.println(_map);
 		assertFalse(_map.containsKey("key1"));
 		assertEquals("value", _map.get("key2"));
+		assertEquals("value", _map.get("key3"));
 	}
 	
 }
