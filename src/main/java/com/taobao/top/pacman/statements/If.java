b66cc05241ca1d27fb75c2f15aea52aebc997599
houkun <houkun@taobao.com>
2014-08-01 21:30:09 +0800

    fix If.Condition null, go 'Else'

--- src/main/java/com/taobao/top/pacman/statements/If.java
+++ src/main/java/com/taobao/top/pacman/statements/If.java
@@ -7,12 +7,12 @@ public class If extends NativeActivity {
 	public InArgument Condition;
 	public Activity Then;
 	public Activity Else;
-
+	
 	public If() {
 		super();
 		this.Condition = new InArgument(true);
 	}
-
+	
 	@Override
 	protected void cacheMetadata(ActivityMetadata metadata) {
 		metadata.bindAndAddArgument(this.Condition,
@@ -20,10 +20,11 @@ public class If extends NativeActivity {
 		metadata.addChild(this.Then);
 		metadata.addChild(this.Else);
 	}
-
+	
 	@Override
 	protected void execute(NativeActivityContext context) {
-		if ((Boolean) this.Condition.get(context)) {
+		Boolean condition = (Boolean) this.Condition.get(context);
+		if (condition != null && condition) {
 			if (this.Then != null)
 				context.scheduleActivity(this.Then);
 			return;
